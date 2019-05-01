bc750b3e62a559d0af3ca0c92cb4ea4cd87ec9bf
Shoni Isra <villajohnny@gmail.com>
2019-05-01 13:19:21 -0500

    proyecto funcional

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject jugador;
+    private Vector3 posicionRelativa;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionRelativa = transform.position - jugador.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = jugador.transform.position + posicionRelativa;
+    }
+}
---
+++ Assets/Scripts/CuboScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+public class CuboScript : MonoBehaviour
+{
+    Rigidbody rb;
+    
+   
+    public float velocidad;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();       
+       
+    }
+    public void FixedUpdate()
+    {
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+        Vector3 movimiento = new Vector3(movimientoHorizontal, 0, movimientoVertical);
+        rb.AddForce(movimiento * velocidad);
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    
+}
